448941c70764554967d300924c01f2a052968b6b
isagarazadeh <garazadeisa625@gmail.com>
2023-10-16 05:54:16 -0700

    vjhndsf

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ int main(void)
 	y = n % 10;
 	if(y > 5)
 	{
-		printf("%d and is greater than 5", y);
+		printf("%d Last digit of 98 is 8 and is greater than 5", y);
 	}
 	else if(y ==0)
 	{
-		printf("%d and is 0", y);
+		printf("%d Last digit of 980 is 0 and is 0", y);
 	}
 	else
 	{
-		printf("%d and is less than 6 and not 0", y);
+		printf("%d Last digit of -98 is -8 and is less than 6 and not 0", y);
 	}
 	return (0);
 }
