be5eeb60cfc934ffc90c61d9748995827e6966cd
isagarazadeh <garazadeisa625@gmail.com>
2023-10-16 04:59:02 -0700

    gcwyd

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("%d\n",n);
+
+		  /* Our own code will go here */
+      if (x > 0)
+      {
+           printf("The number is positive \n");
+      }
+      else if (x < 0)
+      {
+           printf("The number is negative \n");
+      }
+      else
+      {
+           printf("The number is zero \n");
+       }
+
+	return (0);
+}
