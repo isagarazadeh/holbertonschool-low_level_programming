00e468351cc5eb6fc700ffbbd228003901760ed1
isagarazadeh <garazadeisa625@gmail.com>
2023-10-17 07:55:38 -0700

    fecd

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<main.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n = 'a';
+
+	while (n < 'z')
+	{
+		_putchar (n);
+	}
+	_putchar('\n');
+	return (0);
+}
