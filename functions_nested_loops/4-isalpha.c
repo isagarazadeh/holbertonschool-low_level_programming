a60fee8008105924e4be4cc019cadd770e7a5954
isagarazadeh <garazadeisa625@gmail.com>
2023-10-17 09:15:15 -0700

    4 finally

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -6,12 +6,12 @@
  */
 int _isalpha(int c)
 {
-        if ((c > 64 && c < 91) || (c > 96 && c < 123))
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if ((c > 64 && c < 91) || (c > 96 && c < 123))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
