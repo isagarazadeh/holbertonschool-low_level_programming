9cd5e2ad81aebe16ac0720197de04147ae1dd17c
isagarazadeh <garazadeisa625@gmail.com>
2023-10-17 10:28:36 -0700

    fd

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_last_digit -  computes the absolute value of an integer.
+ * @a: int.
+ * Return: void.
+ */
+int print_last_digit(int n)
+{
+	int r;
+
+	r = n % 10;
+	if (r < 0)
+	{
+		r = -1 * r;
+	}
+	_putchar(r + '0');
+
+	return (r);
+}
