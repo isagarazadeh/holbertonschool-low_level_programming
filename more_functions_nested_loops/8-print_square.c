01dd8f7d3a6985c83569ff74fb6da06ee5062aa8
isagarazadeh <garazadeisa625@gmail.com>
2023-10-20 05:50:20 -0700

    8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -19,6 +19,8 @@ void print_square(int size)
                                 _putchar('\n');
                         }
 		}
-		_putchar('\n');
+		if (i < size - 1)
+			_putchar('\n');
 	}
+	_putchar('\n');
 }
