f37cc823030330c74c13fb1a92a63737d99a4355
isagarazadeh <garazadeisa625@gmail.com>
2023-10-23 08:02:56 -0700

    4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - check the code
+ *@s: char.
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		c++;
+	}
+	s--;
+	for (; c > 0; c--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
