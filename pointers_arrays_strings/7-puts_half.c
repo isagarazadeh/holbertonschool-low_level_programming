2b66280229b89149e433c66e49cd815393f23b6a
isagarazadeh <garazadeisa625@gmail.com>
2023-10-24 04:39:58 -0700

    7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - check the code
+ *@str: char.
+ */
+void puts_half(char *str)
+{
+	int a = strlen(str);
+
+	if (a % 2 == 0)
+	{
+		a = a / 2;
+	}
+	else
+	{
+		a = (a - 1) / 2;
+	}
+
+	while (*str != '\0')
+	{
+		_putchar(*(str + a));
+		str++;
+	}
+	_putchar('\n');
+}
