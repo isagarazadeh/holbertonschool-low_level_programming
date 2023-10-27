4ce9d8756607a502ebbd8269e06f8209baa2f8a2
isagarazadeh <garazadeisa625@gmail.com>
2023-10-27 06:16:10 -0700

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - check the code
+ *@s: variable.
+ *Return: Always 0.
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] > 96 && s[i] < 123)
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
