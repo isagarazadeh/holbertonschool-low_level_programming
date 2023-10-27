90331ca4718d78c434ef0b6a406ebc4a50b90282
isagarazadeh <garazadeisa625@gmail.com>
2023-10-27 08:40:28 -0700

    0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n > 0)
+	{
+	*(s + n - 1) = b;
+	n--;
+	}
+	return (s);
+}
