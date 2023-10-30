d8020f83bb4dfd6112a9a9992dda2056181b9eca
isagarazadeh <garazadeisa625@gmail.com>
2023-10-30 06:22:32 -0700

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts_recursion - check the code
+ *@s: string.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	_putchar('\n');
+	return;
+}
