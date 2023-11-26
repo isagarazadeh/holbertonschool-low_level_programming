8227eace52cc963224c1090421712c4f4a227630
isagarazadeh <garazadeisa625@gmail.com>
2023-11-26 09:28:04 -0800

    0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,11 +1,20 @@
-#include <stdlib.h>
+#include <stdarg.h>
 /**
- * print_name - prints name.
- * @name: name.
- * @f: function pointer.
+ * sum_them_all -  sums all arguments.
+ * @n: argument count.
+ * Return: sum.
  */
-void print_name(char *name, void (*f)(char *))
+int sum_them_all(const unsigned int n, ...)
 {
-	if (f != NULL)
-		(*f)(name);
+	va_list ap;
+	int sum = 0;
+	unsigned int i = 0;
+
+	if (n == 0)
+		return (0);
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
 }
