463c49473a64d593f217e849b1dee11951bb313c
isagarazadeh <garazadeisa625@gmail.com>
2023-12-11 04:49:30 -0800

    0

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,18 +1,18 @@
 #include "lists.h"
-#include <stdlib.h>
+
 /**
- * sum_dlistint - finds sum of all elements of list.
- * @head: first node of list.
- * Return: sum.
+ * sum_dlistint - finds the sum of integers
+ *
+ * @head: given linked list
+ *
+ * Return: sum of integers
  */
+
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
 
-	while (temp != NULL)
-	{
-		sum += temp->n;
-		head = head->next;
-	}
+	while (head)
+		sum += head->n, head = head->next;
 	return (sum);
 }
