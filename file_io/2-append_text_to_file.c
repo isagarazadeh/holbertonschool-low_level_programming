ca000a9508911dd4c0dd326b59a195081365ce48
isagarazadeh <garazadeisa625@gmail.com>
2023-12-15 06:14:29 -0800

    2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (!text_content)
 		text_content = "";
-	fd = open(filename, O_RDWR, O_APPEND);
+	fd = open(filename, O_RDWR | O_APPEND);
 	w = write(fd, text_content, strlen(text_content));
 	if (fd == -1 || w == -1)
 	{
